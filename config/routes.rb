f3b757cd253db69afab44f1bab58550582a0697a
rosanna montanaro <rosmontanaro28@gmail.com>
2022-05-17 16:30:34 +0200

    task app in rails almost implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#inde0x"
+
+  # resources :tasks
+
+  get "tasks/new", to: "tasks#new"
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
